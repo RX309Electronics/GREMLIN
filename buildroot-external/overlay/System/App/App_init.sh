32d2ddaa41672bfed207adee3635d1fa2ca96678
RX309Electronics <309Electronics@gmail.com>
2025-12-18 16:32:16 +0100

    Set executable bits for release v0.1-alpha

---
+++ buildroot-external/overlay/System/App/App_init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pip install pygame
+wget https://github.com/RX309Electronics/Dvd-Logo-V2.0/archive/refs/heads/main.zip
+unzip main.zip
+cd Dvd-Logo-V2.0-main/DVD-logo/
+sleep 1
+python3 Main.py
